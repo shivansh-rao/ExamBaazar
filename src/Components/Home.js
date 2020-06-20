0b72ec0054632f55d825ab4bde2c3e8b3916964a
shivansh-rao <shivanshrao464@gmail.com>
2020-06-20 17:29:19 +0530

    question.js modified

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Mapping from './Mapping.js'
-// const style={
-//     color:
-// }
+
 export class Home extends Component {
     constructor(props) {
         super(props)
@@ -25,7 +23,7 @@ export class Home extends Component {
     
     render() {
         const {exams,streams}=this.state
-        // const exam=exams.map((e)=><div key={e._id}>{e.name}</div>)
+        
         return (
             <div className="container">
             <header className="jumbotron">
@@ -33,26 +31,16 @@ export class Home extends Component {
                   <h1 style={{color: "green"}} >
                       ExamBazar!
                   </h1>
-                  {/* <p>
-                      <a href='/campgrounds/new' className="btn btn-primary btn-large">Add your campground..</a>
-                  </p> */}
               </div>
-          </header>
+            </header>
               <div className="row" >
                    { streams.map((c)=>
                       <div key={c._id} className="col-sm-12">
                           <div className="thumbnail">
-                              {/* <img src={c.logo}/> */}
-                              
-                                  <div className="caption">
+                                 <div className="caption">
                                       <h1 style={{color:'blue'}}>
                                           {c.name}
                                       </h1>
-                                     
-                                      
-                                      {/* <p>
-                                          <a href={`/questions/${c._id}`} className="btn btn-primary">Related Questions...</a>
-                                      </p> */}
                                   </div>
                                   <Mapping stream={c._id} exam={exams}/>
                           </div>
--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+// import process.env from 'dotenv'
+// require('dotenv').config()
 export class Questions extends Component {
     constructor(props) {
         super(props)
@@ -7,14 +9,16 @@ export class Questions extends Component {
         localStorage.setItem('pre',JSON.stringify(arr));
         this.state = {
              ques:[{type:'abs'}],
+             abo:[],
              text:''
         }
     }
+
     componentDidMount=()=>{
         
       var  api={
-           api_key:'8107065529',
-           api_secret:'5ee9d77168192338799149e3',
+           api_key: REACT_APP_KEY,
+           api_secret: REACT_APP_SECRET,
            examId:this.props.match.params.id
                 }
             
@@ -25,8 +29,10 @@ export class Questions extends Component {
             }
                 )
         .then(res=>{
-            this.setState({ques:res.data.data.question.questions});
-            var q=this.state.ques;
+            console.log(res.data)
+            this.setState({ques:res.data.data.question.questions,
+                            abo:res.data.data.question});
+            var q=this.state.abo;
             console.log(q);
         })
         .catch(err=>console.log(err))
@@ -39,7 +45,7 @@ export class Questions extends Component {
     next=(e)=>{
         var a=localStorage.getItem('pre');
         var arr=JSON.parse(a);
-        arr.push(this.state.ques)
+        arr.push(this.state.abo)
         localStorage.setItem('pre',JSON.stringify(arr));
          e.preventDefault();
         this.componentDidMount();
@@ -47,62 +53,94 @@ export class Questions extends Component {
 
     prev=()=>{
        var p= localStorage.getItem('pre');
-    //    console.log(p.length)
+    
        var prev=JSON.parse(p);
-    //    console.log(prev.length)
+    
        if(prev.length>=1)
        {var previous=prev.pop();
        localStorage.setItem('pre',JSON.stringify(prev));
-       this.setState({ques:previous});}
+       this.setState({ques:previous.questions,
+                        abo:previous});}
        else{
-        //    console.log(prev)
-           this.setState({ques:[{type:'abs'}]})
+        
+           this.setState({ques:[{type:'abs'}],abo:[]})
        }
     }
     render() {
-        var {ques}=this.state
+        var {ques,abo}=this.state
         var q=this.state.ques
+
+        if(abo.images!=null)
+        {
+            var pic=abo.images.map((im,index)=><div key={index} className="col-sm-12">
+                          <div className="thumbnail">
+                              <img src={im}/></div></div>)
+        }
+
+        else{
+            var pic=<img src={abo.images}/>
+        }
+
         var type=q[0].type;
+
+        //for mcq type
+
         if(type==="mcq")
         {
             return (
             <div>
-                {ques.map((q,index)=>
+                <p>{abo.test}</p>
+                <h1>{abo.context}</h1>
+                <div className="row">{pic}</div>
+                <form onSubmit={this.next}>
+                {   ques.map((q,index)=>
                     <div key={index}>
+                        
                         <h2>{q.question}</h2>
+                           
+                        <h4>Correct:({q.marking.correct}) ,  Incorrect:({q.marking.incorrect})</h4>
                         <hr></hr>
-                        <form onSubmit={this.next}>
-                        {q.options.map((op,index)=>
-                            <div key={index}>
-                             <label ><h3><strong>{index+1}-  </strong><em>{op.option}</em></h3></label>
-                              <input type="checkbox" />
-                              </div>
+                        
+                        {   q.options.map((op,index)=>
+                                <div key={index}>
+                                    <label ><h3><strong>{index+1}   -  </strong><em>{op.option}</em></h3></label>
+                                    <input type="checkbox" />
+                                </div>
                         )}
+                        
+                     </div>
+                )}
                         <input type="submit"></input>
                         </form>
-                     </div>
-                        )}
                         <button onClick={this.next} className="">Next</button>
                         <button onClick={this.prev} className="">Previous</button>
             </div>
         )
                         }
+
+                        //for numerical type
          else{
-                            return(
-            <div>
-            {ques.map((q,index)=>
-                    <div key={index}>
-                        <h2>{q.question}</h2>
-                        <hr></hr>
-                        <form onSubmit={this.next}>
-                        <input type="text" onChange={this.output}></input>
-                        <input type="submit"></input>
-                        </form>
-                     </div>
-            )}
-                        <button onClick={this.next} className="">Next</button>
-                        <button onClick={this.prev} className="">Previous</button>
-            </div>
+             return(
+                    <div>
+                        <p>{abo.test}</p>
+                               <h1>{abo.context}</h1>
+                            <div className="row">{pic}</div>
+                                <form onSubmit={this.next}>
+                                    {   ques.map((q,index)=>
+                                        <div key={index}>
+                                            <h2>{q.question}</h2>
+                                            <h4>Correct:({q.marking.correct})  ,  Incorrect:({q.marking.incorrect})</h4>
+                                            <hr></hr>
+                                
+                                            <input type="text" onChange={this.output} />
+                                
+                                        </div>
+                                    )}
+                                            <input type="submit" />
+                                </form>
+                                            <button onClick={this.next} className="">Next</button>
+                                            <button onClick={this.prev} className="">Previous</button>
+                    </div>
                                 
                             )
                         }
